08cabd236a18fd0f5b4a76d6ba699750513b6c85
iKooru.com <124317926+iKooruHQ@users.noreply.github.com>
2023-08-20 22:09:56 -0700

    Update admin.rs

--- src/admin.rs
+++ src/admin.rs
@@ -1,15 +1,24 @@
 use soroban_sdk::{Env, Address};
 use super::storage_types::{DataKey};
 
-fn check_admin(env: &Env) -> bool {
-    env.invoker() == env.data().get(DataKey::Admin).unwrap().unwrap()
+/// Checks if the invoker is an admin.
+pub fn check_admin(env: &Env) -> bool {
+    match env.data().get::<Address>(DataKey::Admin) {
+        Some(Ok(admin)) => env.invoker() == admin,
+        _ => false,
+    }
 }
 
-fn get_admin(env: &Env) -> Option<Result<Address, ConversionError>> {
-    env.data().get(DataKey::Admin)
+/// Retrieves the address of the admin.
+pub fn get_admin(env: &Env) -> Result<Address, &'static str> {
+    match env.data().get::<Address>(DataKey::Admin) {
+        Some(Ok(admin)) => Ok(admin),
+        Some(Err(_)) => Err("Failed to convert data to address."),
+        None => Err("Admin not set."),
+    }
 }
 
-fn set_admin(env: &Env, admin: Address) {
+/// Sets the address for the admin.
+pub fn set_admin(env: &Env, admin: Address) {
     env.data().set(DataKey::Admin, admin)
 }
-
